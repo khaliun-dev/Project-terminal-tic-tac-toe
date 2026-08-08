f890d54a8b226783aff477371206bc601b237fc4
mandip sanger <mandipsanger1@gmail.com>
2026-08-08 11:41:45 +0100

    statues-checker

--- status-checker.js
+++ status-checker.js
@@ -1,81 +1,116 @@
-import { checkIfNoMovesLeft } from './board-printer.js';
+import { checkIfNoMovesLeft } from "./board-printer.js";
 
 /*
-    Example board:
-        let board = [
-            ['X', '_', '_'],
-            ['_', 'X', '_'],
-            ['O', 'O', 'X']
-        ];
+Example board:
+let board = [
+["X", "*", "*"],
+["*", "X", "*"],
+["O", "O", "X"]
+];
 */
 
 /*
-    Given 3 parameters:
-        - a tic-tac-toe board (array of arrays)
-        - a player ('X' or 'O')
-        - a row index number (0, 1 or 2)
-    Return true if the player has made a move in all 3 squares in the row
-    Otherwise, return false
+Given 3 parameters:
+
+* a tic-tac-toe board
+* a player ("X" or "O")
+* a row index number (0, 1 or 2)
+
+Return true if the player has made a move in all 3 squares in the row.
+Otherwise, return false.
 */
 function checkRow(board, player, rowNumber) {
+  return (
+    board[rowNumber][0] === player &&
+    board[rowNumber][1] === player &&
+    board[rowNumber][2] === player
+  );
 }
 
 /*
-    Given 3 parameters:
-        - a tic-tac-toe board (array of arrays)
-        - a player ('X' or 'O')
-        - a column index number (0, 1 or 2)
-    Return true if the player has made a move in all 3 squares in the column
-    Otherwise, return false
+Test checkRow:
+*/
+const board = [
+  ["X", "*", "*"],
+  ["*", "X", "*"],
+  ["O", "O", "X"],
+];
+
+console.log(checkRow(board, "X", 0)); // false
+console.log(checkRow(board, "X", 1)); // false
+console.log(checkRow(board, "X", 2)); // false
+console.log(checkRow(board, "O", 2)); // false
+
+/*
+Given 3 parameters:
+
+* a tic-tac-toe board
+* a player ("X" or "O")
+* a column index number (0, 1 or 2)
+
+Return true if the player has made a move in all 3 squares in the column.
+Otherwise, return false.
 */
 function checkColumn(board, player, columnNumber) {
+  return (
+    board[0][columnNumber] === player &&
+    board[1][columnNumber] === player &&
+    board[2][columnNumber] === player
+  );
 }
 
 /*
-    Given 2 parameters:
-        - a tic-tac-toe board (array of arrays)
-        - a player ('X' or 'O')
-    Return true if the player has made a move in 3 diagonal squares
-    Otherwise, return false
+Given 2 parameters:
+
+* a tic-tac-toe board
+* a player ("X" or "O")
+
+Return true if the player has made a move in 3 diagonal squares.
+Otherwise, return false.
 */
 function checkDiagonal(board, player) {
-    // It may be easier to use an if statement than a loop here
+  const leftToRight =
+    board[0][0] === player && board[1][1] === player && board[2][2] === player;
+
+  const rightToLeft =
+    board[0][2] === player && board[1][1] === player && board[2][0] === player;
+
+  return leftToRight || rightToLeft;
 }
 
-
 /*
-    There is no need to change any code below this line.
+There is no need to change any code below this line.
 */
 
 function checkIfPlayerWon(board, player) {
-    for(let i = 0; i <= 2; i++) {
-        if(checkRow(board, player, i) || checkColumn(board, player, i)) {
-            return true;
-        }
+  for (let i = 0; i <= 2; i++) {
+    if (checkRow(board, player, i) || checkColumn(board, player, i)) {
+      return true;
     }
+  }
 
-    if(checkDiagonal(board, player)) {
-        return true;
-    }
+  if (checkDiagonal(board, player)) {
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 export function isGameOver(board) {
-    if(checkIfPlayerWon(board, 'X')) {
-        console.log('X has won the game!\n');
-        return true;
-    }
+  if (checkIfPlayerWon(board, "X")) {
+    console.log("X has won the game!\n");
+    return true;
+  }
 
-    if(checkIfPlayerWon(board, 'O')) {
-        console.log('O has won the game!\n');
-        return true;
-    }
+  if (checkIfPlayerWon(board, "O")) {
+    console.log("O has won the game!\n");
+    return true;
+  }
 
-    if(checkIfNoMovesLeft(board)) {
-        console.log('Game Over - It\s a tie!\n');
-        return true;
-    }
+  if (checkIfNoMovesLeft(board)) {
+    console.log("Game Over - It's a tie!\n");
+    return true;
+  }
 
-    return false;
+  return false;
 }
